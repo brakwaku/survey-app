d6ed04bada9f28f1d8428fd6438c805f9df5bb96
Kwaku Appau-Nkansah <brakwakujr@gmail.com>
2021-11-30 16:54:05 -0700

    Heroku setup

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,11 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import { notFound, errorHandler } from './middleware/errorMiddleware.js';
-import connectDB from './config/db.js';
-import cors from 'cors';
+import express from "express";
+import dotenv from "dotenv";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import connectDB from "./config/db.js";
+import cors from "cors";
+import path from "path";
 
-import surveyRoutes from './routes/surveyRoutes.js';
+import surveyRoutes from "./routes/surveyRoutes.js";
 
 dotenv.config();
 
@@ -14,22 +15,35 @@ const app = express();
 
 app.use(express.json());
 
-const corsOptions ={
-    origin:'http://localhost:3000', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
-}
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
 app.use(cors(corsOptions));
 
-app.get('/', (req, res) => {
-    res.send('API is running...')
-});
+app.use("/api/surveys", surveyRoutes);
 
-app.use('/api/surveys', surveyRoutes);
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running...");
+  });
+}
 
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
