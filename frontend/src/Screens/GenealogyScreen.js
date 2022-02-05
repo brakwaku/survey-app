c651ab398eb9b9a3b69bce272c58191621c106f0
Kwaku Appau-Nkansah <brakwakujr@gmail.com>
2022-02-05 00:20:12 -0700

    Typo correction

--- frontend/src/Screens/GenealogyScreen.js
+++ frontend/src/Screens/GenealogyScreen.js
@@ -5,7 +5,7 @@ const GenealogyScreen = () => {
   return (
     <div className="container">
       <div style={{ minHeight: 80 + "vh" }}>
-        <h1>This is the GENEALOGY Link Pagen</h1>
+        <h1>This is the GENEALOGY Link Page</h1>
       </div>
       <SurveyLink buttonName="Page Edit Suggestion" />
     </div>
