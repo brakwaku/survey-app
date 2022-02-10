bf6ec7742000520b99ac6ee05f4645c617f677c7
Kwaku Appau-Nkansah <brakwakujr@gmail.com>
2022-02-10 06:46:00 -0700

    Scroll to top of page on route change

--- frontend/src/Screens/SurveyScreen.js
+++ frontend/src/Screens/SurveyScreen.js
@@ -1,14 +1,25 @@
-import React from "react";
-import SurveyForm from "../components/SurveyForm";
-import styled from "styled-components";
-import media from "../utilities/media";
+import React, { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import SurveyForm from '../components/SurveyForm';
+import styled from 'styled-components';
+import media from '../utilities/media';
 
 const SurveyScreen = () => {
+  // Custom hook that uses both useLocation and useEffect
+  const useScrollToTop = () => {
+    const location = useLocation();
+    useEffect(() => {
+      window.scrollTo({ top: 0 }); // scroll to the top of the browser window when changing route
+    }, [location]);
+  };
+
+  useScrollToTop();
+
   return (
-    <div style={{ minHeight: 90 + "vh" }}  className="container">
+    <div style={{ minHeight: 90 + 'vh' }} className="container">
       <MainWrapper>
         <h1>Page Edit Suggestion</h1>
-        <hr/>
+        <hr />
         <SurveyForm />
       </MainWrapper>
     </div>
